b6ef4ed871c453a4fd1a45e72c4d73ce726bb020
KevinT78 <kevindune@hotmail.fr>
2024-05-13 12:27:29 +0200

    Update du openapi et suppression de code inutile du vcb-16

--- server/Controllers/utilisateurs.controllers.js
+++ server/Controllers/utilisateurs.controllers.js
@@ -1,15 +1,8 @@
-const { validationResult } = require("express-validator");
 const Personne = require("../models/personnes");
 
 module.exports = {
     AllUsers: async (req, res) =>{
-        try{
-            const errors = validationResult(req);
-            if(!errors.isEmpty()){
-                return res.status(422).json({
-                    error: errors.array()[0].msg,
-                });
-            }
+        try{      
             const users = await Personne.find({}, 'id nom prenom ');
             return res.status(200).json(users);
         }catch(err){
