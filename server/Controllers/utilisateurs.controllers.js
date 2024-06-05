6b07f1fd41ec9caa5e54a44f133f09d529f44d31
KevinT78 <kevindune@hotmail.fr>
2024-06-05 14:29:23 +0200

    endpoints pour l'ajout de role et +

--- server/Controllers/utilisateurs.controllers.js
+++ server/Controllers/utilisateurs.controllers.js
@@ -1,14 +1,83 @@
-const Personne = require("../models/personnes");
+const jwtToken = require('jsonwebtoken');
+const { validationResult } = require('express-validator');
+const Personne = require('../models/personnes');
 
 module.exports = {
-    AllUsers: async (req, res) =>{
-        try{      
-            const users = await Personne.find({}, 'id nom prenom ');
+    AllUsers: async (req, res) => {
+        try {      
+            const users = await Personne.find({}, 'id nom prenom');
             return res.status(200).json(users);
-        }catch(err){
+        } catch (err) {
             return res.status(500).json({
                 error: err.message,
             });
         }
-    }
+    },
+
+    addRole: async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(422).json({
+                    error: errors.array()[0].msg,
+                });
+            }
+
+            // Récupération du body de la requête
+            const { role } = req.body;
+
+            // Récupéraion de l'id de l'utilisateur
+            const token = req.cookies.token;
+            if (!token) {
+                return res.status(401).json({ message: "Token manquant" });
+            }
+            
+            let decodedToken;
+            try {
+                decodedToken = jwtToken.verify(token, 'shhhhh');
+            } catch (error) {
+                return res.status(401).json({ message: "Token invalide" });
+            }
+
+            const userId = decodedToken._id;
+    
+
+            // Ajout du rôle à l'utilisateur
+            const personne = await Personne.findOneAndUpdate(
+                { _id: userId },
+                { $set: { role: role } },
+                { new: true, useFindAndModify: false }
+            );
+    
+            if (!personne) {
+                return res.status(404).json({ message: "Utilisateur non trouvé" });
+            }
+    
+            res.status(200).json({ message: "Rôle attribué avec succès"});
+        } catch (error) {
+            console.error("Erreur lors de l'attribution du rôle:", error);
+            res.status(500).json({ message: "Erreur lors de l'attribution du rôle", error: error.message });
+        }
+    },
+
+    getUsersWithRoleChild: async (req, res) => {
+        // Recherche des utilisateur qui ont le role child
+        try {
+            const users = await Personne.find({ role: 'child' }, '_id');
+    
+            if (users.length === 0) {
+                return res.status(404).json({ message: "Aucun utilisateur avec le rôle 'child' trouvé" });
+            }
+    
+            const userIds = users.map(user => user._id);
+    
+            res.status(200).json(userIds);
+        } catch (error) {
+            console.error("Erreur lors de la récupération des utilisateurs:", error);
+            res.status(500).json({
+                message: "Erreur lors de la récupération des utilisateurs",
+                error: error.message,
+            });
+        }
+    },
 }
--- server/Routes/utilisateurs.routes.js
+++ server/Routes/utilisateurs.routes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {AllUsers} = require("../Controllers/utilisateurs.controllers");
+const {AllUsers,addRole,getUsersWithRoleChild} = require("../Controllers/utilisateurs.controllers");
 
 // Route pour récupérer tous les utilisateurs
 router.get("/AllUsers", AllUsers);
+// Route pour ajouter un role à l'utilisateur
+router.put("/addRole", addRole);
+// Route pour récupérer les utilisateurs avec un role enfant
+router.get("/getUsersWithRoleChild", getUsersWithRoleChild)
 module.exports = router;
