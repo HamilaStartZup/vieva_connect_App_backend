e2f7c39e982ae61bcc7d1df56eed34ddda9d5ce8
KevinT78 <kevindune@hotmail.fr>
2025-05-19 11:49:07 +0200

    suppression du serveur principal pour cette branche

--- server/Routes/alertes.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {createAlerte} = require("../Controllers/alertes.controllers"); // Assuming 'alertesController.js' is in the same directory
-
-// Creation d'une alerte
-router.post("/createAlerte", createAlerte);
-
-module.exports = router;
--- server/Routes/messages.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {sendMessage} = require("../Controllers/messages.controllers.js")
-const {isAuthenticated} = require("../Controllers/auth.controllers.js") 
-
-
-
-router.post("/send/:id", isAuthenticated,sendMessage);
-module.exports = router;
